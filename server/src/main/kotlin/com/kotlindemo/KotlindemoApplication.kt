931c49f92720a60f7c4c483944d587815fd3a614
marcelo.filho <marceloasf1999@gmail.com>
2021-07-20 22:40:11 -0300

    Implementation of the kotlin with spring doc sample

--- server/src/main/kotlin/com/kotlindemo/KotlindemoApplication.kt
+++ server/src/main/kotlin/com/kotlindemo/KotlindemoApplication.kt
@@ -2,10 +2,25 @@ package com.kotlindemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlindemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlindemoApplication>(*args)
+    runApplication<KotlindemoApplication>(*args)
+
+    data class Message(val id: String?, val text: String)
+
+    @RestController
+    class MessageResource {
+
+        @GetMapping
+        fun index(): List<Message> = listOf(
+                Message("1", "Hello!"),
+                Message("2", "Bonjour!"),
+                Message("3", "Privet!"),
+        )
+    }
 }
